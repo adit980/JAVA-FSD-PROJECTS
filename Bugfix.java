6ed83144d361c0380e7184bcd916d1594bc1604c
adit980 <109268500+adit980@users.noreply.github.com>
2022-07-28 16:23:38 +0530

    bug fix

--- Bugfix.java
+++ Bugfix.java
@@ -6,7 +6,8 @@ import java.util.Scanner;
 
 public class Bugfix {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         /*System.out.println("Hello World!");*/
         System.out.println("\n**************************************\n");
         System.out.println("\tWelcome to TheDesk \n");
@@ -88,27 +89,86 @@ public class Bugfix {
         }
 
     }
-    private static void closeApp() {
-        System.out.println("Closing your application... \nThank you!");
+    private static void closeApp()
+           {
+                 System.out.println("Closing your application... \nThank you!");
             }
-    private static void searchExpenses(ArrayList<Integer> arrayList) {
+    private static void searchExpenses(ArrayList<Integer> arrayList)
+    {
         int leng = arrayList.size();
+        
         System.out.println("Enter the expense you need to search:\t");
-        
-        Scanner in=new Scanner(System.in);
-        int find= in.nextInt();
-        
-        int res= Collections.binarySearch(arrayList, find);
-        if(res==find)
-        System.out.println(res+" expense is found");
-        else
-        	System.out.println("Expense NOT FOUND!");
+        Scanner st = new Scanner(System.in);
+        int key = st.nextInt();
+        binarySearch(arrayList , 0 , key , leng);
     }
-    private static void sortExpenses(ArrayList<Integer> arrayList) {
-        int arrlength =  arrayList.size();
-        Collections.sort(arrayList);
-        System.out.println(arrayList);
+    private static void sortExpenses(ArrayList<Integer> arrayList) 
+    {
+    	int arrlength = arrayList.size();
+    	sort(arrayList , 0 , arrlength-1);
+    	System.out.println(arrayList);
         
-       //Complete the method. The expenses should be sorted in ascending order.
+       
     }
+    //yhgch
+    
+    public static int partition(ArrayList<Integer> arr, int low, int high)
+    {
+    	//SORTING IS HAPPENING
+      int pivot = arr.get(high);
+      int i = (low-1);
+      for (int j=low; j<high; j++)
+      {
+       if (arr.get(j) <= pivot)
+        {
+          i++;
+          int temp = arr.get(i);
+           arr.set(i, arr.get(j));
+           arr.set(j, temp);
+    }
+    }
+    int temp = arr.get(i + 1);
+    arr.set(i + 1, arr.get(high));
+    arr.set(high, temp);
+    return i+1;
+    }
+    //SORT ENDS
+    public static void sort(ArrayList<Integer> arr, int low, int high)
+    {
+    	//PASSED VALUES FOR QUICK SORT
+    if (low < high)
+    {
+    int pi = partition(arr, low, high);
+    sort(arr, low, pi-1);
+    sort(arr, pi+1, high);
+    }
+    }
+    
+    
+    public static void binarySearch(ArrayList<Integer> arr, int lb, int key, int ub) {
+    	int midValue= (lb+ub)/2;
+    	while(lb<=ub) 
+    	{
+    	 if(arr.get(midValue) <key)
+    	   {
+    	      lb= midValue+1;
+    	   }
+    	else if(arr.get(midValue) ==key)
+    	   {
+    	     System.out.println("Element found at index: "+midValue);
+    	      break;
+    	    }
+    	else
+    	   {
+    	      ub=midValue-1;
+    	   }
+    	 
+    	midValue=(lb+ub)/2;
+    }
+    	    if(lb>ub) 
+    	    {
+    	         System.out.println("Element Not Found");
+    	     }
+ }
+
 }
